48754b332bf041861e63a6e8324d17873a0f2d36
farrah1 <fmughal@timeplay.com>
2017-06-22 15:22:18 -0400

    fixed

--- app/components/App.js
+++ app/components/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.handleClick = this.handleClick.bind(this);
+    this.addToList = this.addToList.bind(this);
     this.deleteItem = this.deleteItem.bind(this);
     this.state = {
       list: [],
@@ -15,17 +16,42 @@ class App extends React.Component {
     };
   }
 
-  handleClick(e) {
-    if (e.key != undefined) {
-      console.log('this is an edit')
-    }
-    else {
+  addToList(e) {
     this.state.list.push(e);
     //add a key to each object
     for (var i=0; i<this.state.list.length; i++) {
       var obj = this.state.list[i];
       obj['key']= i.toString()
     }
+  }
+
+  handleClick(e) {
+  //if a previous key is defined - means this is an edit to an existing list item
+    if (e.key != undefined) {
+      //find the object in the list array that is being editted (find same key)
+      var arr=this.state.list;
+      for (var i=0; i<arr.length; i++) {
+        if (arr[i].key == e.key) {
+          var arr2=arr[i];
+            //if there was no change in priority...
+            if (arr2.priority == e.priority) {
+              //update only the input
+              return arr2.input = e.input;
+            }
+            //if there was a change in priority...
+            else {
+              //remove the current object from the array
+              this.deleteItem(arr2);
+              //add the edit as a new item to the array and assign a new key value
+              this.addToList(e);
+              this.forceUpdate();
+            }
+        }
+      }
+    }
+    //if a key is not assigned - means this is a new added list item
+    else {
+      this.addToList(e);
     }
 
     //sort the array of list items by priority
